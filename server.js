9f9fe5101a562d2533bc683adeb639337e2a3988
Carolina Reedy <carinvid@gmail.com>
2021-07-23 10:46:06 -0600

    adding PORT

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const fs = require("fs");
 
-const adminRoutes = require("./routes/admin");
-const notesRoutes = require("./routes/notes");
+const notesArray = require("./db/db.json");
 
-app.use("/admin", adminRoutes);
-app.use(notesRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
-});
-
-app.listen(3001);
+const PORT = process.env.PORT || 3001;
