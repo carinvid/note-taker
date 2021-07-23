254f589c54c3fede246b6c657a5291581ca04c1f
Carolina Reedy <carinvid@gmail.com>
2021-07-23 10:10:37 -0600

    adding routes

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const notesRoutes = require("./routes/notes");
+
+app.use("/admin", adminRoutes);
+app.use(notesRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3001);
